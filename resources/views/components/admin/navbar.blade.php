00e35775aac5031f5ddaa5fc93d300847ede2578
kotta <kotabarakat382002@gmail.com>
2024-07-27 03:35:54 +0200

    update notification dropdown

--- resources/views/components/admin/navbar.blade.php
+++ resources/views/components/admin/navbar.blade.php
@@ -144,56 +144,26 @@
                 </a>
                 <div class="dropdown-menu p-0" aria-labelledby="notificationDropdown">
                     <div class="px-3 py-2 d-flex align-items-center justify-content-between border-bottom">
-                        <p>6 New Notifications</p>
+                        <p>{{count(auth()->user()->notifications)}} </p>
                         <a href="javascript:;" class="text-muted">Clear all</a>
                     </div>
-    <div class="p-1">
-      <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-        <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                <i class="icon-sm text-white" data-feather="gift"></i>
-        </div>
-        <div class="flex-grow-1 me-2">
-                                <p>New Order Recieved</p>
-                                <p class="tx-12 text-muted">30 min ago</p>
-        </div>
-      </a>
-      <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-        <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                <i class="icon-sm text-white" data-feather="alert-circle"></i>
-        </div>
-        <div class="flex-grow-1 me-2">
-                                <p>Server Limit Reached!</p>
-                                <p class="tx-12 text-muted">1 hrs ago</p>
-        </div>
-      </a>
-      <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-        <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-          <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30" alt="userr">
-        </div>
-        <div class="flex-grow-1 me-2">
-                                <p>New customer registered</p>
-                                <p class="tx-12 text-muted">2 sec ago</p>
-        </div>
-      </a>
-      <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-        <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                <i class="icon-sm text-white" data-feather="layers"></i>
-        </div>
-        <div class="flex-grow-1 me-2">
-                                <p>Apps are ready for update</p>
-                                <p class="tx-12 text-muted">5 hrs ago</p>
-        </div>
-      </a>
-      <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-        <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                <i class="icon-sm text-white" data-feather="download"></i>
-        </div>
-        <div class="flex-grow-1 me-2">
-                                <p>Download completed</p>
-                                <p class="tx-12 text-muted">6 hrs ago</p>
-        </div>
-      </a>
-    </div>
+                        <div class="p-1">
+                            @forelse(auth()->user()->notifications as $notify)
+                            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
+                                <div class="wd-30 ht-30 d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
+                                                        <i class="icon-sm text-white" data-feather="gift"></i>
+                                </div>
+                                <div class="flex-grow-1 me-2">
+                                                        <p>{{$notify->data['message']}}</p>
+                                                        <p class="tx-12 text-muted">{{$notify->created_at}}</p>
+                                </div>
+                            </a>
+                            @empty
+                            <div class="flex-grow-1 me-2">
+                                <p>No Notifications</p>
+                            @endforelse
+
+                        </div>
                     <div class="px-3 py-2 d-flex align-items-center justify-content-center border-top">
                         <a href="javascript:;">View all</a>
                     </div>
